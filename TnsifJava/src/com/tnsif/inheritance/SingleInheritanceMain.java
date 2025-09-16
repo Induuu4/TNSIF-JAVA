2808a5c5ea9861da4cc522de7290b20236a8cc7c
INDHU <INDHU@DESKTOP-O2IM9DT.bbrouter>
2025-09-16 22:27:25 +0530

    Class 4

---
+++ TnsifJava/src/com/tnsif/inheritance/SingleInheritanceMain.java
@@ -0,0 +1,11 @@
+package com.tnsif.inheritance;
+
+public class SingleInheritanceMain {
+	public static void main(String[] args) {
+		B obj = new B();
+		obj.display();
+		System.out.println(obj.a);
+		obj.show();
+	}
+
+}
