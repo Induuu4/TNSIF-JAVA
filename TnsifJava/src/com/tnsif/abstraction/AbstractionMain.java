bd12b422e7fd1ff8f82e583d42b42c1bfcd7bc72
INDHU <INDHU@DESKTOP-O2IM9DT.bbrouter>
2025-09-19 08:58:14 +0530

    Class 6

---
+++ TnsifJava/src/com/tnsif/abstraction/AbstractionMain.java
@@ -0,0 +1,13 @@
+package com.tnsif.abstraction;
+
+public class AbstractionMain {
+
+	public static void main(String[] args) {
+		Square s=new Square(4);
+		s.calArea();
+		s.show();
+		Rectangle r=new Rectangle(4,5);
+		r.calArea();
+		r.show();	
+	}
+}
