1d784bdb434281fc3b72f94652e7cc5f8fe0aa5a
INDHU <INDHU@DESKTOP-O2IM9DT.bbrouter>
2025-10-08 19:45:39 +0530

    jdbc

--- jdbcprograms/src/jdbcprograms/executor/JdbcMain.java
+++ jdbcprograms/src/jdbcprograms/executor/JdbcMain.java
@@ -1,5 +1,14 @@
 package jdbcprograms.executor;
 
+import jdbcprograms.service.CRUD;
+
 public class JdbcMain {
+
+	public static void main(String[] args) {
+		CRUD.addStudent(101, "Sneha");
+		CRUD.addStudent(102, "Shiva");
+		CRUD.addStudent(103, "Priya");
+
+	}
 
 }
--- jdbcprograms/src/jdbcprograms/service/CRUD.java
+++ jdbcprograms/src/jdbcprograms/service/CRUD.java
@@ -1,5 +1,36 @@
 package jdbcprograms.service;
 
-public class CRUD {
+import java.sql.*;
+import javax.sql.*;
+import jdbcprograms.dao.DbUtil;
 
+public class CRUD {
+	static Connection con;
+	static PreparedStatement pst;
+	
+	static {
+		con=DbUtil.getConnection();
+		if(con!=null)
+		{
+			System.out.println("connection is success");
+		}
+	}
+	
+	//created the table now we have to add the values to table
+	public static int addStudent(int sid,String sname)
+	{
+		int n=0;
+		try {
+			pst = con.prepareStatement("INSERT INTO student VALUES(?,?)");
+			pst.setInt(1, sid);
+			pst.setString(2, sname);
+			pst.executeUpdate();
+		}catch(Exception e)
+		{
+			
+		}
+		return n;
+	}
+	
+	
 }
